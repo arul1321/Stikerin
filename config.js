da0577b17aa0826eed40c2b6c5c6df41b03b03d2
IG@Ismailreal.id <77217686+ismailofficial@users.noreply.github.com>
2021-11-22 17:03:32 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,33 +2,33 @@ global.DeveloperMode = 'false' //true Or false
 global.linkGC = ['https://chat.whatsapp.com/Lb4Emjih98rBiCZiZoS2eM', 'https://chat.whatsapp.com/FrluVyY5kBRIhY6yKfH4DI']
 global.Owner = {
   // use the phone number with the country code, for example Indonesia '62' America '1' etc
-  '62857139649630': { // put your number here
-    name: 'Owner Metro Bot/Creator games-wabot',
+  '6283805279906': { // put your number here
+    name: 'Owner Metro Bot/Creator Ismail Kurama',
     isDev: false, // if true this number will send if bot have bug and developer mode true
     isReport: false, // if true this number will send if user use command /report or /join
     isCreator: true // if true this number will send if user use command /owner or /creator
   },
-  '6281390658325': { // put your number here
+  '6283805279906': { // put your number here
     isDev: true, // if true this number will send if bot have bug and developer mode true
     isReport: true, // if true this number will send if user use command /report or /join
     isCreator: false // if true this number will send if user use command /owner or /creator
   },
-  '6282256115584': { // put your number here
-    name: 'Dave',
+  '6283805279906': { // put your number here
+    name: 'muhamad ismail',
     isDev: false, // if true this number will send if bot have bug and developer mode true
     isReport: true, // if true this number will send if user use command /report or /join
     isCreator: false // if true this number will send if user use command /owner or /creator
   },
-  '60199782326': {
-    name: 'Adiixyz',
+  '6283805279906': {
+    name: 'Ismail Nuramsa',
     isReport: true, // if true this number will send if user use command /report or /join
   },
-  '6285713964963': {
+  '6283805279906': {
     isReport: true
   },
-  '13479805233': {} // ignore if you don't want anything, like name dll
+  '6283805279906': {} // ignore if you don't want anything, like name dll
 }
-global.mods = ['13479805233'] // Want some help?
+global.mods = ['6283805279906'] // Want some help?
 global.prems = [] // Premium user has unlimited limit
 global.APIs = { // API Prefix
   // name: 'https://website'
@@ -53,10 +53,10 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.packname = 'wa.me/6285713964963'
-global.author = 'Metro Bot Is The Best'
+global.packname = 'wa.me/6283806279906'
+global.author = 'YT: Ismail Kurama'
 
-global.multiplier = 39 // The higher, The harder levelup
+global.multiplier = 75 // The higher, The harder levelup
 
 let fs = require('fs')
 let chalk = require('chalk')
